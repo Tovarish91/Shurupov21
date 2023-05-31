fa3c4e4826b88dd9d8f45ff56e640fdb83a42305
Tovarish91 <shurupov0dima@gmail.com>
2023-05-31 11:29:46 +0300

    Добавьте файлы проекта.

---
+++ Shurupov21/BuyerInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shurupov21
+{
+    internal class BuyerInfo : Client
+    {
+        public string buyerPhoneNumber { get; set; }
+        public string buyerEmail { get; set; }
+        public string buyerCreditCardNumber { get; set; }
+        public BuyerInfo(int id, string name, string surname, string patronymic, DateTime birthday, char gender, int age, string phone, string email, string creditCardNumber)
+                     : base(id, name, surname, patronymic, birthday, gender, age)  // параметры передаются в конструктор базового класса, т.е. при вызове конструктора данного класса мы вызовем и заполним поля базового класса Person
+        {
+            buyerPhoneNumber = phone;
+            buyerEmail = email;
+            buyerCreditCardNumber = creditCardNumber;
+        }
+    }
+}
---
+++ Shurupov21/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shurupov21
+{
+    internal class Client
+    {
+        public int IdPerson { get; set; }
+        public string PersonName { get; set; }
+        public string PersonSurname { get; set; }
+        public string PersonPatronymic { get; set; }
+        public DateTime PersonBirthday { get; set; }
+        public char PersonGender { get; set; }
+        public int PersonAge { get; set; }
+
+        public Client(int id, string name, string surname, string patronymic, DateTime birthday, char gender, int age) //Можем задать значения через конструктор класса.
+
+        {
+            PersonName = name;
+            PersonAge = age;
+            PersonSurname = surname;
+            PersonPatronymic = patronymic;
+            PersonBirthday = birthday;
+            PersonGender = gender;
+            IdPerson = id;
+        }
+    }
+}
